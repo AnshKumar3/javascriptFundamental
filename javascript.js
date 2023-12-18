7c4072e7ab45bb293429df8413a5412a0215f7f3
ansh <vaibhavchaurasia768@gmail.com>
2023-12-18 10:41:05 +0530

    first commit

--- javascript.js
+++ javascript.js
@@ -1,2 +1,9 @@
-alert("conected")
 
+/*let MyHobby = "coding"
+//let FavPet = "dog"
+//console.log(`FavHobbyis ==${MyHobby}`)
+//console.log(`FavFoodIs ==${FavFood}`)
+///console.log(`FavPetIs ==${FavPet}`)*/
+let FavFood=prompt("enter your favrite food");
+console.log(`My Fav Food is ${FavFood}`)
+alert(`mujhai pasand hai ${FavFood}`)
